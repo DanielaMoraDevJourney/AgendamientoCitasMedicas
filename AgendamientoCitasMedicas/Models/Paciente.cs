3d559d6577850cb0e911b3dd701057466fa7a766
Juan Diego Silva Castillo <juan.silva.castillo@udla.edu.ec>
2024-06-12 21:23:22 -0500

    Agregar archivos de proyecto.

---
+++ AgendamientoCitasMedicas/Models/Paciente.cs
@@ -0,0 +1,39 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace AgendamientoCitasMedicas.Models
+{
+    public class Paciente
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        public DateTime Birthday { get; set; }
+
+        [Required]
+        public TipoGenero Genre { get; set; }
+
+        [Required]
+        public string? Address{ get; set; }
+
+        [Required]
+        public string? Name { get; set; }
+
+        [Required]
+
+        public string? Phone { get; set; }
+
+
+        public List<CitaMedica>? CitaMedicaList { get; set; }
+
+        public List<Tratamiento>? Tratamientos { get; set; }
+
+        public HistorialMedico? HistorialMedico { get; set;}
+    }
+
+
+    public enum TipoGenero{
+        Masculino, 
+        Femenino
+    }
+}
---
+++ AgendamientoCitasMedicas/Models/Tratamiento.cs
@@ -0,0 +1,26 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace AgendamientoCitasMedicas.Models
+{
+    public class Tratamiento
+    {
+        [Key]
+
+        public int Id { get; set; }
+
+        [Required]
+        public string? Description { get; set; }
+
+        [Required]
+        public string? Dosage { get; set; }
+
+        [Required]
+        public string? Contraindication { get; set; }
+
+        public int PacienteId { get; set; }
+        public Paciente? Paciente { get; set; }
+
+        public int MedicoId { get; set; }
+        public Medico? Medico { get; set; }
+    }
+}
