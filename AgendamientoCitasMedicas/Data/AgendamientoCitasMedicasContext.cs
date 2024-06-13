c1d94d6241fe5f74a8d84e2266d18cce64ac5f12
Juan Diego Silva Castillo <juan.silva.castillo@udla.edu.ec>
2024-06-12 21:33:04 -0500

    Controladores

---
+++ AgendamientoCitasMedicas/Data/AgendamientoCitasMedicasContext.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using AgendamientoCitasMedicas.Models;
+
+namespace AgendamientoCitasMedicas.Data
+{
+    public class AgendamientoCitasMedicasContext : DbContext
+    {
+        public AgendamientoCitasMedicasContext (DbContextOptions<AgendamientoCitasMedicasContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<AgendamientoCitasMedicas.Models.CitaMedica> CitaMedica { get; set; } = default!;
+        public DbSet<AgendamientoCitasMedicas.Models.HistorialMedico> HistorialMedico { get; set; } = default!;
+        public DbSet<AgendamientoCitasMedicas.Models.Medico> Medico { get; set; } = default!;
+        public DbSet<AgendamientoCitasMedicas.Models.Paciente> Paciente { get; set; } = default!;
+        public DbSet<AgendamientoCitasMedicas.Models.Reporte> Reporte { get; set; } = default!;
+        public DbSet<AgendamientoCitasMedicas.Models.Tratamiento> Tratamiento { get; set; } = default!;
+    }
+}
--- AgendamientoCitasMedicas/Program.cs
+++ AgendamientoCitasMedicas/Program.cs
@@ -1,4 +1,9 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using AgendamientoCitasMedicas.Data;
 var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddDbContext<AgendamientoCitasMedicasContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("AgendamientoCitasMedicasContext") ?? throw new InvalidOperationException("Connection string 'AgendamientoCitasMedicasContext' not found.")));
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
